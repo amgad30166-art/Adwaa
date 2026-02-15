1933ab542484c04820adf7f2ca87be6219b984c9
amgad30166-art <amgad30166@gmail.com>
2026-02-15 19:03:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-// Ready for future features
-console.log("Adhwa Alradef Recruitment Website Loaded");
+document.getElementById("whatsappForm").addEventListener("submit", function(e){
+e.preventDefault();
 
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let workerType = document.getElementById("workerType").value;
+let country = document.getElementById("country").value;
+
+let message = `طلب استقدام جديد:
+الاسم: ${name}
+الجوال: ${phone}
+نوع العمالة: ${workerType}
+الدولة: ${country}`;
+
+let url = "https://wa.me/966543281015?text=" + encodeURIComponent(message);
+window.open(url, "_blank");
+});
